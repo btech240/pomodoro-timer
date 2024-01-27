9530b2b20acd2c783b9f5134411a1a464d0994d3
btech240 <brenttlong@gmail.com>
2024-01-27 14:57:17 -0500

    Added text to canvas and background colors

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ LONG_BREAK_MIN = 20
 # Create the User Interface
 window = Tk()
 window.title("Pomodoro")
-# window.minsize(width=600, height=400)
+window.config(padx=100, pady=50, bg=YELLOW)
 
 # Create the canvas for the image
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, background=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 32, "bold"))
 canvas.pack()
 
 # Mainloop has to be at the end of the code
